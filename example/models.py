fe2d25213246ccb9009b57342fc065ce3451dcdd
ThiagoAZ <thiagoaugustozeferino@gmail.com>
2021-01-21 12:22:33 -0300

    Mudanças Da Ultima Aula

--- example/models.py
+++ example/models.py
@@ -1,22 +1,31 @@
 from django.db import models
 
-class Person(models.Model):
-    id = models.AutoField(primary_key = True)
+class Reporter(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
-    age = models.IntegerField(default = None, blank = True)
-    SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, default = None)
+    email = models.EmailField()
 
-class Musician(Person):
-    instrument = models.CharField(max_length = 100)
+    def chamar(self):
+        print(f'{self.first_name} {self.last_name}')
 
-class Album(models.Model):
-    artist = models.ForeignKey(Musician, on_delete = models.CASCADE)
-    name = models.CharField(max_length = 100)
-    released_date = models.DateField()
-    num_stars = models.IntegerField()
+class Publication(models.Model):
+    title = models.CharField(max_length = 30)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length = 100)
+    pub_date = models.DateTimeField()
+    reporter = models.ForeignKey(Reporter, on_delete = models.CASCADE)
+    publications = models.ManyToManyField(Publication)
+
+    def __str__(self):
+        return self.headline
+
+class Meta:
+    ordering = ['headline']
